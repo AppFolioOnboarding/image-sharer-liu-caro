ef487b26737f2c72c7efd856a9dece4a06172259
Caroline Liu <caroline.liu@appfolio.com>
2021-01-14 11:20:31 -0800

    Complete issue 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#home'
+  root 'image_links#index'
 
   resources :feedbacks, only: [:new]
+
+  resources :image_links, only: [:index]
 
   namespace :api do
     resource :feedbacks, only: [:create]
