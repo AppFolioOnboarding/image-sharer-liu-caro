13b3c675ed281fce43285b4ba5b4592559311adc
Caroline Liu <caroline.liu@appfolio.com>
2021-01-21 14:00:02 -0800

    Add ImageLinks to fixtures

--- app/models/image_link.rb
+++ app/models/image_link.rb
@@ -1,6 +1,6 @@
 class ImageLink < ApplicationRecord
   validates :url, presence: true, format: {
-    with: %r{/((https?):\/\/)?(www.)?[a-z0-9]+\.[a-z]+(\/[a-zA-Z0-9#]+\/?)*/},
+    with: %r{/((https?):\/\/)?[a-z0-9]*/},
     message: 'Please enter a valid URL'
   }
 end
